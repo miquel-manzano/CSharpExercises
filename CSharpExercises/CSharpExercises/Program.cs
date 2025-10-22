9c46f048a7305400023726629fae883f1b72d994
✨ <miquel.manzano.7e6@itb.cat>
2025-10-22 22:08:57 +0200

    Exercise one done

--- CSharpExercises/CSharpExercises/Program.cs
+++ CSharpExercises/CSharpExercises/Program.cs
@@ -2,6 +2,14 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Enter a value to check if it's a natural number:");
+        if (int.TryParse(Console.ReadLine(), out int value))
+        {
+            bool isNatural = value >= 0;
+            if (isNatural)
+                Console.WriteLine($"{value} is a natural number.");
+            else
+                Console.WriteLine($"{value} is NOT a natural number.");
+        }
     }
 }
