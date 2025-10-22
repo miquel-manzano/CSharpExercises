73684cbd08813ae51a08d908cf0a66a3716f82ea
✨ <miquel.manzano.7e6@itb.cat>
2025-10-22 22:12:07 +0200

    Exercise six done

--- CSharpExercises/CSharpExercises/Program.cs
+++ CSharpExercises/CSharpExercises/Program.cs
@@ -2,6 +2,34 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        const string USERNAME = "root";
+        const string PASSWORD = "toor";
+        const int MAX_ATTEMPTS = 3;
+        int currentAttempts = 1;
+        bool accessGranted = false;
+
+        while (currentAttempts <= MAX_ATTEMPTS && !accessGranted)
+        {
+            Console.WriteLine($"Attempt {currentAttempts} of {MAX_ATTEMPTS}");
+            Console.Write("Enter username: ");
+            string username = Console.ReadLine();
+            Console.Write("Enter password: ");
+            string password = Console.ReadLine();
+
+            if (username == USERNAME && password == PASSWORD)
+            {
+                accessGranted = true;
+            }
+            else
+            {
+                Console.WriteLine("Error: Username and/or password incorrect.");
+                currentAttempts++;
+            }
+        }
+
+        if (accessGranted)
+            Console.WriteLine("Access granted");
+        else
+            Console.WriteLine("This incident will be reported.");
     }
 }
