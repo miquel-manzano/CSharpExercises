b8b3dcab2db8d8594f592006b4ccb53b7655ee11
✨ <miquel.manzano.7e6@itb.cat>
2025-10-22 22:10:37 +0200

    Exercise three done

--- CSharpExercises/CSharpExercises/Program.cs
+++ CSharpExercises/CSharpExercises/Program.cs
@@ -2,6 +2,29 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        const int MAX_ATTEMPTS = 3;
+        int attempts = 1;
+        bool isValid = false;
+        int number = 0;
+
+        while (attempts <= MAX_ATTEMPTS && !isValid)
+        {
+            Console.WriteLine($"Attempt {attempts} of {MAX_ATTEMPTS}");
+            Console.WriteLine("Enter an integer number within the interval (2, 30):");
+            if (int.TryParse(Console.ReadLine(), out number) && number > 2 && number < 30)
+            {
+                isValid = true;
+            }
+            else
+            {
+                Console.WriteLine("Error: The number is not within the interval (2, 30).");
+                attempts++;
+            }
+        }
+
+        if (isValid)
+            Console.WriteLine($"Valid number entered: {number}");
+        else
+            Console.WriteLine("You have lost. No valid number entered.");
     }
 }
