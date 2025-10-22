302d014eca0bfc559859fcd43d3381fa4b1f9eb7
✨ <miquel.manzano.7e6@itb.cat>
2025-10-22 22:11:35 +0200

    Exercise five done

--- CSharpExercises/CSharpExercises/Program.cs
+++ CSharpExercises/CSharpExercises/Program.cs
@@ -2,6 +2,23 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Enter a natural number (maximum 7 digits, max 9999999):");
+        if (int.TryParse(Console.ReadLine(), out int decimalNumber))
+        {
+            if (decimalNumber < 0)
+            {
+                Console.WriteLine("Error: You must enter a natural number.");
+                return;
+            }
+
+            if (decimalNumber > 9999999)
+            {
+                Console.WriteLine("Error: You must enter a number with a maximum of 7 digits.");
+                return;
+            }
+
+            string binary = Convert.ToString(decimalNumber, 2);
+            Console.WriteLine($"{decimalNumber} in binary is equal to: {binary}");
+        }
     }
 }
