0df33777ccc615e4c9e1d328bad8d2a778ec876a
✨ <miquel.manzano.7e6@itb.cat>
2025-10-22 22:11:06 +0200

    Exercise four done

--- CSharpExercises/CSharpExercises/Program.cs
+++ CSharpExercises/CSharpExercises/Program.cs
@@ -2,6 +2,25 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Enter a natural number (positive integer or zero):");
+        if (int.TryParse(Console.ReadLine(), out int originalNumber))
+        {
+            if (originalNumber < 0)
+            {
+                Console.WriteLine("Error: You must enter a natural number.");
+                return;
+            }
+
+            int number = originalNumber;
+            int counter = (number == 0) ? 1 : 0;
+
+            while (number > 0)
+            {
+                number /= 10;
+                counter++;
+            }
+
+            Console.WriteLine($"Number of digits of {originalNumber}: {counter}");
+        }
     }
 }
