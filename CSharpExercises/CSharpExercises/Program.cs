92009a34489dca7d15b0d6cc9712624feb712cf9
✨ <miquel.manzano.7e6@itb.cat>
2025-10-22 22:14:06 +0200

    Exercise nine done

--- CSharpExercises/CSharpExercises/Program.cs
+++ CSharpExercises/CSharpExercises/Program.cs
@@ -2,6 +2,27 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.Write("How many numbers do you want to enter? ");
+        if (int.TryParse(Console.ReadLine(), out int numberQuantity))
+        {
+            int positiveCount = 0, negativeCount = 0, zeroCount = 0;
+
+            for (int i = 1; i <= numberQuantity; i++)
+            {
+                Console.Write($"Enter number {i}/{numberQuantity}: ");
+                if (int.TryParse(Console.ReadLine(), out int currentNumber))
+                {
+                    if (currentNumber > 0) positiveCount++;
+                    else if (currentNumber < 0) negativeCount++;
+                    else zeroCount++;
+                }
+            }
+
+            Console.WriteLine("--- RESULT ---");
+            Console.WriteLine($"Total numbers entered: {numberQuantity}");
+            Console.WriteLine($"Positive numbers: {positiveCount}");
+            Console.WriteLine($"Negative numbers: {negativeCount}");
+            Console.WriteLine($"Zeros: {zeroCount}");
+        }
     }
 }
