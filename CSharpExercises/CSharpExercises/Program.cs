fa8caf430bb7dd9255487fb94d58945b6a312505
✨ <miquel.manzano.7e6@itb.cat>
2025-10-22 22:09:47 +0200

    Exercise two done

--- CSharpExercises/CSharpExercises/Program.cs
+++ CSharpExercises/CSharpExercises/Program.cs
@@ -2,6 +2,13 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Enter a value to check if it's within the interval (5, 50):");
+        if (int.TryParse(Console.ReadLine(), out int number))
+        {
+            if (number > 5 && number < 50)
+                Console.WriteLine($"{number} is within the interval (5, 50).");
+            else
+                Console.WriteLine($"{number} is NOT within the interval (5, 50).");
+        }
     }
 }
